76a3047cbf70f2c8c9e44e0f81458d16d0364c7c
X A <seedgs@qq.com>
2021-11-26 14:25:08 +0800

    文件重定向

--- Memory/C++控制文件.c
+++ Memory/C++控制文件.c
@@ -5,7 +5,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 
-int main(void)
+int main666(void)
 {
 	FILE* num = NULL;//声明一个文件指针(也就是初始化为0)
 	num = fopen("C:\\Users\\huanghui\\source\\repos\\Memory\\Memory\\test.txt", "w+");//创建文件,如果只有文件名，文件的创建会在所属的根目录里面
@@ -17,7 +17,6 @@ int main(void)
 	}
 
 	fputs("654321", num);//fputs(）写入的是系统的缓冲区，而不是直接写入，目的是为了减少硬盘读写次数，从而延缓硬盘使用寿命
-
 
 	fclose(num);//关闭文件，同时把缓冲区内的内容写进文件
 	num = NULL;//重新初始化
---
+++ Memory/文件重定向.c
@@ -0,0 +1,26 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<windows.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main(void)
+{
+
+	char num[10] = { 0 };
+
+	//gets(num);//把键盘输入的内容存储到 num[100]数组中；
+				//因为get()函数没有规定长度，如果输入超过规定的长度，内存就会溢出，导致报错
+
+	fgets(num, 10, stdin);//fget()函数是把键盘（stdin参数）内容存储到 num数组内，并规定长度为10
+	printf("通过键盘存储至数组：%s", num);
+
+	fputs("\n直接显示至显示器：", stdout);
+	fputs(num, stdout);
+	
+
+	//system(num);
+	printf("\n");
+	system("pause");
+	return 0;
+}
